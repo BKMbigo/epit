12a04cf304a7258cc8035788cea123d6a5b742f6
bkmbigo <bkmbigo@gmail.com>
2024-01-01 21:55:24 +0300

    feat: voyager extension functions

--- plugin-build/epit/src/main/kotlin/epit/dsl/voyager/EpitVoyagerScope.kt
+++ plugin-build/epit/src/main/kotlin/epit/dsl/voyager/EpitVoyagerScope.kt
@@ -1,9 +1,8 @@
 package epit.dsl.voyager
 
-import epit.EpitDependency
+import Epit
 import epit.annotations.EpitDsl
 import epit.annotations.ExperimentalEpitApi
-import epit.annotations.InvalidScopeEpitDependency
 import epit.utils.joinWithColon
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 
@@ -12,6 +11,81 @@ import org.gradle.kotlin.dsl.DependencyHandlerScope
 class EpitVoyagerScope internal constructor(
     internal val voyagerVersion: String
 ) {
+
+    @ExperimentalEpitApi
+    val Epit.voyager_navigator
+        get() = Voyager.voyager_navigator.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_navigator(version: String) =
+        Voyager.voyager_navigator.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_bottom_sheet_navigator
+        get() = Voyager.voyager_bottom_sheet_navigator.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_bottom_sheet_navigator(version: String) =
+        Voyager.voyager_bottom_sheet_navigator.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_tab_navigator
+        get() = Voyager.voyager_tab_navigator.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_tab_navigator(version: String) =
+        Voyager.voyager_tab_navigator.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_transitions
+        get() = Voyager.voyager_transitions.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_transitions(version: String) =
+        Voyager.voyager_transitions.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_androidx
+        get() = Voyager.voyager_androidx.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_androidx(version: String) =
+        Voyager.voyager_androidx.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_koin
+        get() = Voyager.voyager_koin.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_koin(version: String) =
+        Voyager.voyager_koin.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_hilt
+        get() = Voyager.voyager_hilt.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_hilt(version: String) =
+        Voyager.voyager_hilt.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_kodein
+        get() = Voyager.voyager_kodein.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_kodein(version: String) =
+        Voyager.voyager_kodein.dependencyAsString(version)
+
+    @ExperimentalEpitApi
+    val Epit.voyager_rxjava
+        get() = Voyager.voyager_rxjava.dependencyAsString
+
+    @ExperimentalEpitApi
+    fun Epit.voyager_rxjava(version: String) =
+        Voyager.voyager_rxjava.dependencyAsString(version)
+
+    /* Internal Functions */
+
     @ExperimentalEpitApi
     val Voyager.dependencyAsString
         get() = moduleName joinWithColon voyagerVersion
@@ -19,14 +93,9 @@ class EpitVoyagerScope internal constructor(
     @ExperimentalEpitApi
     fun Voyager.dependencyAsString(version: String) = moduleName joinWithColon version
 
+    @Deprecated("This method has been deprecated in favor of implementation")
     @ExperimentalEpitApi
     fun DependencyHandlerScope.implementation(voyager: Voyager) {
         add("implementation", voyager.dependencyAsString)
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    @InvalidScopeEpitDependency
-    fun DependencyHandlerScope.implementation(epitDependency: EpitDependency) {
-        throw IllegalStateException("You have called a dependency from the wrong scope. Please refer to Epit documentation for reference")
     }
 }
---
+++ plugin-build/epit/src/test/kotlin/dsl/voyager/VoyagerTests.kt
@@ -0,0 +1,63 @@
+package dsl.voyager
+
+import Epit
+import epit.annotations.ExperimentalEpitApi
+import epit.dsl.epitPreview
+import epit.dsl.voyager.Voyager
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertTrue
+
+@OptIn(ExperimentalEpitApi::class)
+class VoyagerTests {
+
+    @Test
+    fun `verify that voyager block implementation adds dependencies`() {
+        val project = ProjectBuilder.builder().build()
+        project.pluginManager.apply("io.github.bkmbigo.epit")
+
+        val config = project.configurations.create("implementation")
+        val customVersionConfig = project.configurations.create("customVersionImplementation")
+
+        fun DependencyHandlerScope.implementation(dependency: String) {
+            add("implementation", dependency)
+        }
+
+        fun DependencyHandlerScope.customVersionImplementation(dependency: String) {
+            add("customVersionImplementation", dependency)
+        }
+
+        project.dependencies {
+            epitPreview {
+                voyager("1.0.0") {
+                    implementation(Epit.voyager_navigator)
+                    customVersionImplementation(Epit.voyager_navigator("1.1.2"))
+                }
+            }
+        }
+
+        val expectedDependencies = listOf(
+            Voyager.voyager_navigator.moduleName
+        )
+
+        assertContentEquals(
+            expectedDependencies,
+            config.dependencies.map { "${it.group}:${it.name}" },
+            "voyager implementation does not add dependencies"
+        )
+
+        assertContentEquals(
+            expectedDependencies,
+            customVersionConfig.dependencies.map { "${it.group}:${it.name}" },
+            "voyager implementation does not add dependencies"
+        )
+
+        assertTrue(
+            customVersionConfig.dependencies.map { it.version }.all { it == "1.1.2" },
+            "voyager implementation does not add dependencies"
+        )
+    }
+}
